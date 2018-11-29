e295094c1b5fbe720500f67da13e7ad0f7e2c735
Andres Cabrera <andrescabsi@gmail.com>
2018-11-28 20:54:44 -0500

    Base app. Data connection mock

---
+++ src/js/service.js
@@ -0,0 +1,9 @@
+import taskList from '../data/taskList'
+
+export default function(endpoint) {
+  //   return fetch(endpoint).then(data => {
+  //     taskList = data.json()
+  //     return taskList
+  //   })
+  return taskList
+}
